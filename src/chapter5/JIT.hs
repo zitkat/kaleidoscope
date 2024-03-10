624e9a2908ee700c8bc6279911bf9e96ce6f773f
zitkat <tozitka@gmail.com>
2024-03-10 22:22:38 +0100

    In JIT evaluate last defined main.* or main function.

--- src/chapter5/JIT.hs
+++ src/chapter5/JIT.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module JIT where
 
 import Data.Int
 import Data.Word
 import Foreign.Ptr ( FunPtr, castFunPtr )
 import Data.String ( IsString(fromString) )
-import qualified Data.ByteString as BS
+import Data.Maybe
+import qualified Data.ByteString.Char8 as BS
 import Control.Monad.Except
+import Control.Applicative
 
 import LLVM.Target
 import LLVM.Context
@@ -19,6 +23,8 @@ import LLVM.Analysis
 
 import qualified LLVM.ExecutionEngine as EE
 import LLVM.Internal.Analysis (verify)
+import GHC.Generics (Generic1(from1))
+import LLVM.Internal.ExecutionEngine (ExecutableModule, ExecutionEngine)
 
 foreign import ccall "dynamic" haskFun :: FunPtr (IO Double) -> (IO Double)
 
@@ -39,7 +45,7 @@ passes = defaultCuratedPassSetSpec { optLevel = Just 3 }
 runJIT :: AST.Module -> IO AST.Module
 runJIT mod = do
   withContext $ \context ->
-    jit context $ \executionEngine -> 
+    jit context $ \executionEngine ->
       withModuleFromAST context mod $ \m ->
         withPassManager passes $ \pm -> do
           -- Optimization Pass
@@ -50,8 +56,9 @@ runJIT mod = do
           BS.putStrLn s
 
           EE.withModuleInEngine executionEngine m $ \ee -> do
-            mainfn <- EE.getFunction ee (AST.Name $ fromString "main")
-            case mainfn of
+            mainfn <- EE.getFunction ee (fromString "main")
+            mainfn_n <- getFunctionN ee "main"
+            case mainfn_n <|> mainfn of
               Just fn -> do
                 res <- run fn
                 putStrLn $ "Evaluated to: " ++ show res
@@ -59,3 +66,17 @@ runJIT mod = do
 
           -- Return the optimized module
           return optmod
+
+getFunctionN :: (ExecutionEngine e f) => ExecutableModule e -> String -> IO (Maybe f)
+getFunctionN ee name = gFs >>= return . takeFs >>= return . mayLast
+                    where -- gF :: Int -> IO (Maybe f) - TODO ScopedTypeVariables do not work in this case
+                          gF i = EE.getFunction ee (fromString (gFN name i))
+                          -- gFs :: IO [Maybe f]
+                          gFs = mapM gF [1..1000]  -- TODO make this able to work with infinite list, some hints
+                          takeFs fs = sequence $ takeWhile isJust fs
+                          mayLast ml = ml >>= listToMaybe . reverse
+                          
+
+
+gFN :: String -> Int -> String
+gFN name i = name ++ "." ++ show i
