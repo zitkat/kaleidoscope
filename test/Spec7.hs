6c74fab41e2f429f9bc8e40abe4c182a368346ed
zitkat <tozitka@gmail.com>
2024-06-02 00:16:54 +0200

    build: Add test suites for chapter3 and chapter7
    Added test suite configurations for chapter3 and chapter7 to run
    specific unit tests for those chapters. Updated the cabal file to
    include these test suites along with necessary dependencies.

---
+++ test/Spec7.hs
@@ -0,0 +1,18 @@
+import Parser
+import Emit ( codegen )
+import Codegen ( emptyModule ) 
+import qualified LLVM.AST as AST
+
+main :: IO ()
+main = process mod "1 + 1;" >> return ()
+    where 
+        mod = emptyModule "my cool test"
+
+process :: AST.Module -> String -> IO (Maybe AST.Module)
+process modo source = do
+  let res = parseToplevel source
+  case res of
+    Left err -> print err >> return Nothing
+    Right ex -> do
+      ast <- codegen modo ex
+      return $ Just ast
